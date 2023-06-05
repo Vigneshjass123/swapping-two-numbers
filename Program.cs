e2b1f81beefe9942c216e18774caf9092f670a00
Cyntexia <Cyntexia@DESKTOP-R5AAUJH>
2023-06-05 23:04:14 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swapping_two_numbers_without_using_third_variable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            {
+                Console.WriteLine("a value is " + a +" b values is " + b);
+            }
+            Console.ReadKey();
+        }
+    }
+}
